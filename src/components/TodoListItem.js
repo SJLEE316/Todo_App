72372d342e435d256a321942d7269898f09dcbfc
SJLEE316 <lsj8249@naver.com>
2021-01-29 18:04:37 +0900

    최적화하기

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -6,25 +6,27 @@ import {
 } from 'react-icons/md';
 import '../scss/TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      {/* 원하는 className 꼴 : checkbox checked */}
-      {/* <div className={classNames(`checkbox`, { checked })}> 틀린 꼴
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        {/* 원하는 className 꼴 : checkbox checked */}
+        {/* <div className={classNames(`checkbox`, { checked })}> 틀린 꼴
       <div className={checked ? 'checkbox checked' : 'checkbox'}> classNames이용한 꼴 */}
-      <div
-        className={`checkbox ${checked ? 'checked' : ''}`}
-        onClick={() => onToggle(id)}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline></MdRemoveCircleOutline>
+        <div
+          className={`checkbox ${checked ? 'checked' : ''}`}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline></MdRemoveCircleOutline>
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
